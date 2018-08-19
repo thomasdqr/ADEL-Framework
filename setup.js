c45c655aa43a8004f3a54772e38eaa0b84d224c8
thomasdqr <32330439+thomasdqr@users.noreply.github.com>
2018-08-19 20:40:53 +0200

    new version

--- setup.js
+++ setup.js
@@ -2,27 +2,8 @@
 
 var app = new ADEL();
 
-// Optional parameters sent to our app
-app.params.loadAllAtStart = true;
-
-// Enabling navigation bar;
-app.params.useNavBar = true;
-
-
-// Define navbar icons highlight style
-app.params.navBarActiveStyle = {
-	"background-color": "dodgerblue",
-};
-app.params.navBarInactiveStyle = {
-	"background-color": "grey",
-};
-
-
-
 // Initialise our pages
-app.navController.Init(["page1.html", "page2.html"]);
-
-
+app.navController.Init(["page1.html"]);
 
 
 // Example of manual navigation
